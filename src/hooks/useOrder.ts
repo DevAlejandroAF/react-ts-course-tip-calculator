f403d2135eb6ab8eca6e7664915c6d7584b51d5f
DevAlejandroAF <alejandroavi95@hotmail.com>
2024-07-11 16:35:38 -0500

    section 9, class 129

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import type { MenuItem, OrderItem } from "../types";
+
+export default function useOrder() {
+  const [order, setOrder] = useState<OrderItem[]>([]);
+  const [tip, setTip] = useState(0);
+
+  const addItem = (item: MenuItem) => {
+    const existingItem = order.find(orderItem => orderItem.id === item.id);
+    if (existingItem) {
+      const updatedOrder = order.map(orderItem => {
+        if (orderItem.id === item.id) {
+          return {...orderItem, quantity: orderItem.quantity + 1};
+        } else {
+          return orderItem;
+        }
+      });
+      setOrder(updatedOrder);
+    } else {
+      const newItem = {...item, quantity: 1};
+      setOrder([...order, newItem]);
+    }
+  }
+
+  const removeItem = (id: MenuItem['id']) => {
+    setOrder(order.filter(item => item.id !== id));
+  }
+
+  const placeOrder = () => {
+    setOrder([]);
+    setTip(0);
+  }
+
+  return {
+    order,
+    tip,
+    setTip,
+    addItem,
+    removeItem,
+    placeOrder,
+  }
+}
+
